31b7351155710f782d68745726d5aef3c6b50407
minder309 <minder309@gmil.com>
2022-12-20 16:35:52 +0800

    取出資料做h1

--- front/show_wisk.php
+++ front/show_wisk.php
@@ -1,5 +1,8 @@
 <h1>show wisk 頁面。</h1>
 <?php
-$sql="select "
-
+$wisk_num=$_GET['wisk'];
+$sql="select name from winery_name where `id`='$wisk_num'";
+$Winery_name=$pdo->query($sql)->fetchColumn();
+                                // 取出單字。
+dd($Winery_name);
 ?>
