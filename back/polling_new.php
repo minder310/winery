478464c1a91012880bd6f2707efe7ecebcc91e25
minder309 <minder309@gmil.com>
2022-12-28 15:31:58 +0800

    jquree 新增刪除選項完成。

--- back/polling_new.php
+++ back/polling_new.php
@@ -14,6 +14,7 @@
     <div class="opt-div">
         <label for="">選項</label>
         <input type="text" name="opt[]">
+        <button type="button" id="del">刪除選項</button>
     </div>
     <div>
         <input type="submit" value="儲存">
@@ -24,17 +25,27 @@
 
 <script>
     $(function(){
-        // 綁定div。
+        // 綁定區。
+        const form= $('form');
         const div = $('.opt-div');
-        // 綁定啟動按鍵
-        const but = $('button[name=newinput]')
+        const but = $('button[name=newinput]');
+        const dle = $('#del');
         // 顯示出綁定的div的內容。
         console.log ('div',div);
         // 複製物件，不放在function
-        const inpClone=div.clone();
+        console.log ("div",div)
+        let a = 1;
         but.click(function(){
-            // 前面是獎物件貼到哪邊。
-            inpClone.appendTo('.opt-div');
+            // 前面是將物件貼到哪邊，後面是貼什麼物件。
+            const inpClone=$('.opt-div').last().clone(true);
+            div.append(inpClone);
         })
+
+        dle.click(function(){
+             console.log('OK in function');
+             $(this).parent().remove();
+        })
+        
+        
     })
 </script>
