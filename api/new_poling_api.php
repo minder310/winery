7a9dfa8c2da1bdc83d03082ed61b7f06b149a096
pierre.K <minder309@gmail.com>
2023-01-07 17:17:17 +0800

    new_poling_api產生兩個id問題解決。

--- api/new_poling_api.php
+++ api/new_poling_api.php
@@ -1,39 +1,35 @@
 
 <?php
 include "../db/db.php";
-$a =$_POST;
+$a = $_POST;
 dd($a);
-// 傳post，主題。
-$theme[]=$_POST['theme'];
-// 傳是否啟動。
-$on_off[]=$_POST['on_off'];
+// 傳主題與是否啟動。
+$theme_onoff = [$_POST['theme'], $_POST['on_off']];
 // 傳選項。
-$opt=$_POST['opt'];
+$opt = $_POST['opt'];
 // 宣告要輸入的表單，表格名稱。
-$poling_name[]="poling_name";
+$poling_name_onoff = ["poling_name", "no_off"];
 // 宣告需要的
-$on_off_name[]="no_off";
-InPutWiskName("poling",$poling_name,$theme);
-InPutWiskName("poling",$on_off_name,$on_off);
+InPutWiskName("poling", $poling_name_onoff, $theme_onoff);
 
 // 取出最id最大值。
-$Maxidsql=ReadToTable("MAX(id)","poling");
+$Maxidsql = ReadToTable("MAX(id)", "poling");
 // dd($Maxidsql);
-$maxid=ToSql($Maxidsql);
-$bigid[]=$maxid[0]["MAX(id)"];
-dd($bigid);
+$maxid = ToSql($Maxidsql);
+$bigid[] = $maxid[0]["MAX(id)"];
+// dd($bigid);
 
 
 // 宣告要導入的表格。
-$option=["poling_id","option_name"];
+$option = ["poling_id", "option_name"];
 
 // 用foretch輸入多組選項，進入投票系統。
-foreach($opt as $key => $value){
+foreach ($opt as $key => $value) {
 
-    $bigid[1]=$value;
+    $bigid[1] = $value;
     dd($bigid);
-    InPutWiskName("poling_option",$option,$bigid);
-    
+    InPutWiskName("poling_option", $option, $bigid);
 }
-// InPutWiskName("poling_option",$option,$bigid)
+
+echo "請製作回投票成功頁面。";
 ?>
