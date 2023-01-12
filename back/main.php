6c40cccd44b8f2f154d22ebbf07b3a2bce76ed07
minder309 <minder309@gmil.com>
2023-01-12 11:15:37 +0800

    調整圖片大小完成。

--- back/main.php
+++ back/main.php
@@ -1,4 +1,6 @@
+<!-- 宣告取出資料庫內容。 -->
 <?php
+  // 取出最大值id。
   $maxNumber=ToSql(ReadToTable("MAX(id)","whisky_name"));
   $num=$maxNumber[0]["MAX(id)"];
   $whisk;
@@ -12,10 +14,8 @@
     $read[]=ToSql($whisk[$i]);
   }
   // dd($read);
-  
-  
-  
 ?>
+<!-- 取出資料結束。 -->
   <div class="mt-3 col-md-12 col-lg-4 col row justify-content-center">
     <div class="card" style="width:400px">
       <img  class="card-img-top" src="./upimg/<?= $read[0][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
@@ -38,7 +38,7 @@
 </div>
 <div class="mt-3 col-md-12 col-lg-4 col row justify-content-center">
   <div class="card" style="width:400px">
-    <img class="card-img-top" src="./upimg/<?= $read[2][0]["jpge_where"]?>" alt="Card image" style="width:261px ;height:261px;">
+    <img class="card-img-top" src="./upimg/<?= $read[2][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
     <div class="card-body">
       <h4 class="card-title"><?= $read[2][0]["whiskey_brand"]?></h4>
       <p class="card-text">價格:<?= $read[2][0]["price"]?>元</p>
