a5b939b14283bfd8d34498c4dab9c3837f502020
minder309 <minder309@gmil.com>
2023-01-12 14:04:56 +0800

    調整icon

--- back/main.php
+++ back/main.php
@@ -18,7 +18,7 @@
 <!-- 取出資料結束。 -->
   <div class="mt-3 col-md-12 col-lg-4 col row justify-content-center">
     <div class="card" style="width:400px">
-      <img  class="card-img-top" src="./upimg/<?= $read[0][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
+      <img  class="card-img-top mx-auto" src="./upimg/<?= $read[0][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
       <div class="card-body">
         <h4 class="card-title"><?= $read[0][0]["whiskey_brand"]?></h4>
         <p class="card-text">價格:<?= $read[0][0]["price"]?>元</p>
@@ -28,7 +28,7 @@
   </div>
 <div class="mt-3 col-md-12 col-lg-4 col row justify-content-center">
   <div class="card" style="width:400px">
-    <img class="card-img-top" src="./upimg/<?= $read[1][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
+    <img class="card-img-top mx-auto" src="./upimg/<?= $read[1][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
     <div class="card-body">
       <h4 class="card-title"><?= $read[1][0]["whiskey_brand"]?></h4>
       <p class="card-text">價格:<?= $read[1][0]["price"]?>元</p>
@@ -37,9 +37,9 @@
   </div>
 </div>
 <div class="mt-3 col-md-12 col-lg-4 col row justify-content-center">
-  <div class="card" style="width:400px">
-    <img class="card-img-top" src="./upimg/<?= $read[2][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
-    <div class="card-body">
+  <div class="card " style="width:400px">
+    <img class="card-img-top mx-auto" src="./upimg/<?= $read[2][0]["jpge_where"]?>" alt="Card image" style="width:261px; height:261px;">
+    <div class="card-body ">
       <h4 class="card-title"><?= $read[2][0]["whiskey_brand"]?></h4>
       <p class="card-text">價格:<?= $read[2][0]["price"]?>元</p>
       <a href="#" class="btn btn-primary">詳細資料</a>
--- layouts/header.php
+++ layouts/header.php
@@ -39,7 +39,7 @@
 <?php
 if (!$_SESSION['login']) {
 ?>
-    <div class="dropdown row mx-auto container">
+    <div class="dropdown row mx-auto container ">
         <button type="button" class="btn btn-link col col-sm-2"><a href="./index.php?do=main">回首頁</a></button>
         <button type="button" class="btn btn-link col col-sm-2"><a href="./index.php?do=login">登入</a></button>
         <button type="button" class="btn btn-link col col-sm-2"><a href="#">討論區</a></button>
@@ -49,23 +49,22 @@ if (!$_SESSION['login']) {
         </button>
         <ul class="dropdown-menu">
             <?php foreach ($alldate as $key => $value) { ?>
-                <li><a class="dropdown-item" href="./index.php?do=show_wisk_list&wisk=<?=($key+1)?>"><?= $value['name'] ?></a></li>
+                <li><a class="dropdown-item" href="./index.php?do=show_wisk_list&wisk=<?= ($key + 1) ?>"><?= $value['name'] ?></a></li>
             <?php } ?>
         </ul>
     </div>
 <?php } else { ?>
-    <div class="dropdown row mx-auto container">
-        <button type="button" class="btn btn-link col col-sm-2 "><a  href="./admin_center.php">回首頁</a></button>
-        <button type="button" class="btn btn-link col col-sm-2 "><a  href="./logout.php">登出</a></button>
-        <button type="button" class="btn btn-link col col-sm-2 "><a  href="#">討論區</a></button>
-        <button type="button" class="btn btn-link col col-sm-2 "><a  href="./admin_center.php?do=polling_new">投票區</a></button>
-        <button type="button" class="btn btn-link col col-sm-2 "><a  href="./admin_center.php?do=polling_show">投票區</a></button>
-        <button type="button" class="btn btn-link dropdown-toggle col col-sm-2 " data-bs-toggle="dropdown">
-            威士忌品牌
+    <div class="dropdown row mx-auto container bg-danger rounded">
+        <button type="button" class="btn btn-link col col-sm-2 offset-md-1"><a href="./admin_center.php"><img title="首頁" style="width: 24px; height:24px" src="./icon/icons8-木屋-96.png" alt=""></a></button>
+        <button type="button" class="btn btn-link col col-sm-2 "><a href="./logout.php"><img title="登出" style="width:24px;height:24px" src="./icon/icons8-登录右圆角-96.png"></a></button>
+        <button type="button" class="btn btn-link col col-sm-2 "><a href="./admin_center.php?do=polling_new"><img title="天氣" style="height: 24px;width:24px;" src="./icon/icons8-cloudshot.gif" alt=""></a></button>
+        <button type="button" class="btn btn-link col col-sm-2 "><a href="./admin_center.php?do=polling_show"><img title="投票管理" style="width: 24px;height:24px;" src="./icon/icons8-票-96.png" alt=""></a></button>
+        <button type="button" class="btn btn-link dropdown-toggle col col-sm-2  " data-bs-toggle="dropdown">
+            <img title="酒廠介紹" style="width: 24px; height:24px;" src="./icon/icons8-葡萄酒瓶-96.png" alt="">
         </button>
         <ul class="dropdown-menu">
-            <?php foreach ($alldate as $key => $value) {?>
-                <li><a class="dropdown-item" href="./admin_center.php?do=show_wisk_list&wisk=<?=($key+1)?>"><?= $value['name'] ?></a></li>
+            <?php foreach ($alldate as $key => $value) { ?>
+                <li><a class="dropdown-item" href="./admin_center.php?do=show_wisk_list&wisk=<?= ($key + 1) ?>"><?= $value['name'] ?></a></li>
             <?php } ?>
             <li><a class="dropdown-item" href="./admin_center.php?do=InTheNewWisk">新增威士忌</a></li>
         </ul>
