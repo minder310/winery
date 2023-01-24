1812036b1c96b25755a2923dee5a63344bacaa6f
pierre.K <minder309@gmail.com>
2023-01-25 01:44:44 +0800

    修正表單，裝飾

--- back/polling_new.php
+++ back/polling_new.php
@@ -1,27 +1,27 @@
 <h1>投票區</h1>
 <div class="col-12 col-md-4 mx-auto">
-    <div class="mx-auto col-4 col-md-4">
+    <div class="mx-auto col-4 col-md-4 row">
         <button class="btn btn-warning mx-auto" name="newinput">新增選項</button>
     </div>
     <form action="./api/new_poling_api.php" method="POST">
-        <div class="mb-3">
-            <label class="form-label" for="">主題</label>
+        <div class="col-mb-3 row">
+            <label class="form-label col-8" for="">主題</label>
             <input class="form-control" type="text" name="theme">
         </div>
-        <div class="mb-3">
-            <label class="form-label" for="">啟動</label>
-            <input type="radio" name="on_off" id="" value="1">
-            <label class="form-label" for="">關閉</label>
-            <input type="radio" name="on_off" id="" value="0">
+        <div class="col-mb-3 row">
+            <label class="form-label col-2 mx-auto" for="">啟動</label>
+            <input style="height: 15px;" class="col-1 mt-2 mx-auto" type="radio" name="on_off" id="" value="1">
+            <label class="form-label col-2 mx-auto" for="">關閉</label>
+            <input style="height: 15px;" class="col-1 mt-2 mx-auto" type="radio" name="on_off" id="" value="0">
         </div>
-        <div class="opt-div mb-3">
+        <div class="opt-div col-mb-3 row">
             <label class="form-label" for="">選項</label>
-            <input class="form-control" type="text" name="opt[]">
-            <button class="btn btn-warning" type="button" id="del">刪除選項</button>
+            <input class="form-control col-8" type="text" name="opt[]">
+            <button class="btn btn-warning mx-auto col-4 mt-2" type="button" id="del">刪除選項</button>
         </div>
-        <div class="mb-3">
-            <input class="btn btn-warning" type="submit" value="儲存">
-            <input class="btn btn-warning" type="reset" value="清空">
+        <div class="col-mb-3 row mt-2">
+            <input class="btn btn-warning col-4 mx-auto mt-2" type="submit" value="儲存">
+            <input class="btn btn-warning col-4 mx-auto mt-2" type="reset" value="清空">
         </div>
 
     </form>
@@ -35,9 +35,9 @@
         const but = $('button[name=newinput]');
         const dle = $('#del');
         // 顯示出綁定的div的內容。
-        console.log('div', div);
+        console.log("div", div);
         // 複製物件，不放在function
-        console.log("div", div)
+        console.log("div", div);
         let a = 1;
         but.click(function() {
             // 前面是將物件貼到哪邊，後面是貼什麼物件。
--- back/polling_show.php
+++ back/polling_show.php
@@ -1,16 +1,33 @@
 <h1>顯示現有投票。</h1>
 <?php
 // 要顯示的標題跟，內容SQL宣告。
-$sqlPolingName=ReadToTable("*","poling");
+$sqlPolingName = ReadToTable("*", "poling");
 // 載進SQl中。
-$polingName=ToSql($sqlPolingName);
+$polingName = ToSql($sqlPolingName);
 dd($polingName);
 
 ?>
-<ul>
-    <?php foreach($polingName as $key => $value){?>
-        <a href="./admin_center.php?do=polling_show_list&id=<?=$value['id'];?>">
-            <ol><?=$value['poling_name']?></ol>    
-        </a>
-    <?php } ?>
-</ul>    
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col">投票主題</th>
+            <th scope="col">開始時間</th>
+            <th scope="col">結束時間</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($polingName as $key => $value) { ?>
+            <tr>
+                <th scope="row"><?= $value['id'] ?></th>
+                <td>
+                <a href="./admin_center.php?do=polling_show_list&id=<?= $value['id']; ?>">
+                    <?= $value['poling_name'] ?>
+                </a>
+                </td>
+                <td><?= $value['new_date'] ?></td>
+                <td><?= $value['over_date'] ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
